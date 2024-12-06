11680afa4816f0d3a1b145ca37abbd5882a8b0ea
DevJulio <julioocesarmoura2008@gmail.com>
2024-12-06 14:01:55 -0300

    Delete IntegratorProject/java directory

--- IntegratorProject/java/com/api/viagem/dto/ViagemDTO.java
+++
@@ -1,5 +0,0 @@
-package com.api.viagem.dto;
-
-public record ViagemDTO(String Nome, String Telefone, String CPF, String Email, String Origem, String Destino, int Numero_voo, String imgURL) {
-
-}
--- IntegratorProject/java/com/api/viagem/repository/ViagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.viagem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.viagem.model.ViagemModel;
-
-public interface ViagemRepository extends JpaRepository<ViagemModel, Long> {
-
-}
